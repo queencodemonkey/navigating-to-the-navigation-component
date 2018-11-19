429ee060cef3e3d8f38d656b0dc3963b156f50a2
Joshua Jamison <joshuajamison@Joshua-Jamisons-MacBook-Pro-5.local>
2018-11-20 00:09:51 +0100

    disabled login logic

--- nnc-modules/app/src/main/java/rt/nnc/modules/MainActivity.kt
+++ nnc-modules/app/src/main/java/rt/nnc/modules/MainActivity.kt
@@ -23,9 +23,14 @@ class MainActivity : AppCompatActivity(), LoginHandler, ToolbarHandler {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        refreshNavGraph()
+
+        toolbar.setupWithNavController(navController, AppBarConfiguration(navController.graph))
     }
 
+    /**
+     * This is for supporting dynamic toolbar title's since it's not currently supported
+     * https://issuetracker.google.com/issues/80267266
+     */
     override fun setToolbarTitle(title: String) {
         toolbar.title = title
     }
@@ -33,21 +38,23 @@ class MainActivity : AppCompatActivity(), LoginHandler, ToolbarHandler {
     override fun login() {
         val prefs = PreferenceManager.getDefaultSharedPreferences(this)
         prefs.edit().putBoolean(IS_LOGGED_IN, true).apply()
-        refreshNavGraph()
+        // TODO Changing nav graphs is currently broken. https://issuetracker.google.com/issues/111450672
+        //refreshNavGraph()
     }
 
     override fun logout() {
         val prefs = PreferenceManager.getDefaultSharedPreferences(this)
         prefs.edit().remove(IS_LOGGED_IN).apply()
-        refreshNavGraph()
+        // TODO Changing nav graphs is currently broken. https://issuetracker.google.com/issues/111450672
+        // refreshNavGraph()
     }
 
+    // TODO Don't use this. Changing graphs is currently broken. https://issuetracker.google.com/issues/111450672
     private fun refreshNavGraph() {
         if (isLoggedIn()) {
             // setup regular nav graph
             navController.setGraph(R.navigation.nav_graph)
             navController.navigate(navController.graph.startDestination)
-            toolbar.setupWithNavController(navController, AppBarConfiguration(navController.graph))
             toolbar.visibility = VISIBLE
         } else {
             // setup login graph
